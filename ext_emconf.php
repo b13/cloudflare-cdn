9966601e3fbf0e769a770ec3bec1f0fa7f2edf17
Achim Fritz <achim.fritz@b13.de>
2023-10-19 13:45:10 +0200

    [TASK] ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,10 +15,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'typo3@b13.com',
     'state' => 'stable',
     'author_company' => 'b13 GmbH',
-    'version' => '1.0.0',
+    'version' => '2.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-11.5.99',
+            'typo3' => '11.5.0-12.4.99',
         ],
         'conflicts' => [
         ],
